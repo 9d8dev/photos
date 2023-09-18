800ac841dde9166ce8263e163633f3aa13476b8b
Bridger Tower <bridgertower@gmail.com>
2023-09-18 15:33:33 -0600

    add footer links

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ const footer = () => {
   return (
     <footer className="max-w-screen-md px-6 md:px-4 flex flex-col gap-4 lg:gap- lg:py-12 py-8">
       <p>All photos © {data.name} | 2023--Present</p>
-      <p>Design and Code by Bridger and Cameron at 9d8.dev.</p>
+      <p>Design and Code by <a href="https://bridger.to">Bridger</a> and <a href="https://cameronyoungblood.com">Cameron</a> at <a href="https://9d8.dev">9d8.dev</a>.</p>
     </footer>
   );
 };
