1795516df6df16b03133be5edd14ab80e8799770
Bridger Tower <bridgertower@gmail.com>
2023-09-18 20:54:01 -0600

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,17 @@ import "./globals.css";
 import data from "@/utils/settings.json";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import openGraphImage from "@/public/og-image.jpg";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: `Photography by ${data.name}`,
   description: data.description,
+  openGraph: {
+    ...openGraphImage,
+    title: `Photography by ${data.name}`,
+  },
 };
 
 export default function RootLayout({
